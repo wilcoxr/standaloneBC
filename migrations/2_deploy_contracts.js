26a09d969df3c9edbd0a038bf0205a88a0caeca4
wilcoxr3 <wilcoxr3@miamioh.edu>
2023-03-02 09:45:19 -0500

    added goerli support

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,6 @@
 const Marketplace = artifacts.require("Marketplace");
+//const LazyNFT = artifacts.require("LazyNFT");
 module.exports = function(deployer) {
     deployer.deploy(Marketplace);
+    //deployer.deploy(LazyNFT);
   };
